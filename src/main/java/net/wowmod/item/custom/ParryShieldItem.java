73f4a9958c2c951f5a156a54a5690b139e9d107a
lrks89 <kendoka89@gmail.com>
2025-11-27 14:57:09 -0500

    fixed shield blocking animation

--- src/main/java/net/wowmod/item/custom/ParryShieldItem.java
+++ src/main/java/net/wowmod/item/custom/ParryShieldItem.java
@@ -1,5 +1,6 @@
 package net.wowmod.item.custom;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShieldItem;
@@ -20,6 +21,11 @@ public class ParryShieldItem extends ShieldItem {
     @Override
     public UseAction getUseAction(ItemStack stack) {
         return UseAction.BLOCK;
+    }
+
+    @Override
+    public int getMaxUseTime(ItemStack stack, LivingEntity user) {
+        return 72000;
     }
 
     @Override
